2e93c0bd5e098cc80ece31dc4680305aea8b893e
MD RAIHAN JOY <raihanjoy.us@gmail.com>
2026-07-03 15:29:52 +1200

    Add files via upload
    - added  __init__ method to initialize the shared data (date, staff ID, staff name, requisition ID, total, status, approval reference) from Assessment  - Used a global counter outside the class to generate unique requisition ID  - Added add_requisition to collect staff details and items, calculating the running total  - Added approve_requisition to approve totals under $500 automatically and marks totals which are more than $500 as Pending   - Fixed Error in __init__ method as I did not declare global counter declaration when generating the requisition ID inside add_requisition, Initially it was just counter.  - Fixed another Error which was caused while updating total_pending and total_approved without the self. prefix inside approve_requisition, which Python was treating as new local variables instead of class attributes - Design principle used: Implementation and Testing( Implemented the planning and how i have used the data accordingly from the requirements analysis, and through testing I have found the problems in the code and fixed it)

--- final_assessment_Semester1/requisition_prototype.py
+++ final_assessment_Semester1/requisition_prototype.py
@@ -1,3 +1,41 @@
 class Requisition:
-    def __init__(self):
-        pass
+    counter=10000 #Counter to create req id
+    total_submitted=0 #To increment after each requisition submitted for statistics
+    total_pending=0 #to increment or decrement based on requirement for statistics
+    total_approved=0 #to increment or decrement based on requirement for statistics
+    total_not_approved=0 #for statistics
+    def __init__(self,date, staff_name, staff_id):
+        global counter
+        self.date = date
+        self.staff_name= staff_name
+        self.staff_id= staff_id
+        self.status = "Pending"
+        self.id= counter
+        self.total= 0
+        self.approval_reference_ID="N/A"
+        Requisition.total_submitted +=1
+        Requisition.total_pending +=1
+    def add_requisition(self):
+        print("-------Submit Requisition-------")
+        self.date= input("Enter Submission Date (DD/MM/YYYY): ")
+        self.staff_name= input("Enter Staff Name: ")
+        self.staff_id= input("Enter Staff ID: ")
+        print("--------Add Item--------")
+        item_name= input("Enter Item Name: ")
+        item_price= input("Enter Item Price:$")
+        self.total+=item_price
+        more_items= input("Any Other Items You Would Like to Add? (yes/no): :").lower() #to avoid error if there are capitalization errors
+        while more_items == "yes":
+            item_name= input("Enter Item Name: ")
+            item_price= input("Enter Item Price:$")
+            self.total+=item_price
+            more_items= input("Any Other Items You Would Like to Add? (yes/no): :").lower()
+        print("Total: $", self.total)
+    def approve_requisition(self):
+        if self.total<500:
+            self.status="Approved"
+            self.approval_reference_ID= self.staff_id + str(self.id)[-3:]
+            self.total_pending-=1
+            self.total_approved+=1
+        else:
+            self.status="Pending"
